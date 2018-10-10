1ae82ee2e206ee5ecd44fa70a982021b0bd586be
Reynir Stefánsson <Reynir@Reynirs-MacBook-Pro.local>
2018-10-10 16:47:03 +0000

    negative virkar i calculator og test

--- calculator.js
+++ calculator.js
@@ -1,11 +1,31 @@
+
+var numbers = "1,-4,-9";
+
 function add (numbers){
 	
 	if(numbers.length === 0){
 	return 0;
 	}
 	
-	else if(numbers.includes(",")){
-		var numberArray = numbers.split(",");
+
+	
+	else if(numbers.includes(",") || numbers.includes("\n")){
+		var numberArray = numbers.split(/[\n,]+/);
+		
+		if(numbers.includes("-")){
+			var errorList = "";
+			for(var i = 0; i < numberArray.length; i++){
+				if(numberArray[i].includes("-")){
+					if(errorList == ""){
+						errorList += numberArray[i];
+					}
+					else{
+						errorList += (", " +  numberArray[i]);
+					}
+				}
+			}
+			throw "Negatives not allowed: " + errorList;
+		}
 		return sum(numberArray);
 	}
 	
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const add = require("./calculator");
-
+/*
 it("Should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 });
@@ -9,9 +9,16 @@ it("Should return number when only one number is in the string", () => {
 });
 
 it("Should return sum of two given numbers", () => {
-	expect(add("1,2")).toBe(3);
+	expect(add("1\n2")).toBe(3);
 });
 
 it("Should return sum of multiple numbers", () => {
-	expect(add("1,2,3")).toBe(6);
+	expect(add("1\n2\n3")).toBe(6);
+});
+*/
+
+it("Should return negative not allowed with -4 and 99", () => {
+	expect(() => {
+		add("1\n-4\n-9")
+	}).toThrow();
 });
